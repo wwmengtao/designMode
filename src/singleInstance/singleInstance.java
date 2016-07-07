a560878092ad83994a7c6428a8380a11da741232
mengtao1 <mengtao1@lenovo.com>
2016-07-07 17:39:52 +0800

    补充
    Signed-off-by: mengtao1 <mengtao1@lenovo.com>

--- src/singleInstance/singleInstance.java
+++ src/singleInstance/singleInstance.java
@@ -1,7 +1,7 @@
 package singleInstance;
 
 public class singleInstance {
-	public static volatile singleInstance mSingleInstance;//为什么加volatile？
+	public static volatile singleInstance mSingleInstance;//为什么加volatile？提示：双重检查锁定
 	
 	private singleInstance(){}
 	
